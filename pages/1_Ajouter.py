21253ba5b22ace89a795bbd0c49dd42e2a89cb57
Chauve <thomaschauve.sci@disroot.org>
2023-08-24 10:46:05 +0000

    add map

--- pages/1_Ajouter.py
+++ pages/1_Ajouter.py
@@ -10,4 +10,8 @@ f_date=st.date_input('Date',value=st.session_state['d_date'], label_visibility="
 
 id=list(st.session_state['data_bpf'].Ville).index(add_code)
 
-st.write('You selected:', id)
+fig_map = px.scatter_mapbox(st.session_state['data_bpf'].loc[index], lat=index[4], lon=index[3], hover_name=index[0], hover_data=[index[1], index[2]],
+                        color_discrete_sequence=["blue"], zoom=4)
+fig_map.update_layout(mapbox_style="open-street-map")
+
+st.plotly_chart(fig_map, use_container_width=True, sharing="streamlit")
