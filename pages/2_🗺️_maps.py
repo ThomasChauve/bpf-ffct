96feb7ab4c69f707e14ff5b19b47a059b8cfd97b
Chauve <thomaschauve.sci@disroot.org>
2023-08-24 11:14:46 +0000

    sss

--- pages/2_🗺️_maps.py
+++ pages/2_🗺️_maps.py
@@ -5,6 +5,9 @@ import plotly.express as px
 st.set_page_config(page_title="Cartes des BPF",layout="wide", page_icon="🗺️")
 
 
+my_bpf=st.checkbox('Visité', value=True label_visibility="visible")
+not_my_bpf=st.checkbox('Non visité', value=True label_visibility="visible")
+
 index=st.session_state['data_bpf'].columns
 
 fig_map = px.scatter_mapbox(st.session_state['data_bpf'], lat=index[4], lon=index[3], hover_name=index[0], hover_data=[index[1], index[2]],
