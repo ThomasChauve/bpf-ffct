733cb17e4ac4807610c4ebd443082dde3636e105
Chauve <thomaschauve.sci@disroot.org>
2023-08-24 10:23:27 +0000

    fix

--- pages/2_maps.py
+++ pages/2_maps.py
@@ -5,9 +5,9 @@ import plotly.express as px
 st.set_page_config(page_title="Cartes des BPF",layout="wide", page_icon="📊")
 
 
-index=self.cols.columns
+index=st.session_state['data_bpf'].columns
 
-fig_map = px.scatter_mapbox(st.session_state['data_bpf'], lat=index[4], lon=index[3], hover_name=index[1], hover_data=[index[0], index[2],index[6]],
+fig_map = px.scatter_mapbox(st.session_state['data_bpf'], lat=index[4], lon=index[3], hover_name=index[0], hover_data=[index[1], index[2]],
                         color_discrete_sequence=["blue"], zoom=4)
 fig_map.update_layout(mapbox_style="open-street-map")
 
