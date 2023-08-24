3ddae6674adab9a9eaaaf655a380ccbac9a8057c
Chauve <thomaschauve.sci@disroot.org>
2023-08-24 10:27:42 +0000

    add pages add

---
+++ pages/1_Ajouter.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import numpy as np
+import plotly.express as px
+
+st.set_page_config(page_title="Ajouter un BPF",layout="wide", page_icon="📊")
+
+add_code = st.selectbox('Selectionner le BPF',st.session_state['data_bpf'])
