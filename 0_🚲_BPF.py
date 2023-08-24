0f680b2450e769db10cd38fbd72b25a2d852b8aa
Chauve <thomaschauve.sci@disroot.org>
2023-08-24 21:47:42 +0200

    Update 0_🚲_BPF.py

--- 0_🚲_BPF.py
+++ 0_🚲_BPF.py
@@ -38,7 +38,7 @@ with st.sidebar:
     st.session_state['data_bpf'].to_csv('tmp_bpf.csv')
     st.download_button('Télécharger les données .csv',data=Path('tmp_bpf.csv').read_text(),file_name='list_BPF.csv')
 
-uploaded_file=st.file_uploader('Charger fichier ".csv"',accept_multiple_files=True, type='csv', label_visibility="visible")
+uploaded_file=st.file_uploader('Charger fichier ".csv"',accept_multiple_files=False, type='csv', label_visibility="visible")
 if uploaded_file is not None:
     for f in uploaded_file:
-        st.session_state['data_bpf'].append(data_load(f))
+        st.session_state['data_bpf']=data_load(f)
