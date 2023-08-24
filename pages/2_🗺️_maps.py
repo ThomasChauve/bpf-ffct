0c48b46a3909f6be20becf646fa6dd8e78e54129
Chauve <thomaschauve.sci@disroot.org>
2023-08-24 11:11:27 +0000

    sss

---
+++ pages/2_🗺️_maps.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import numpy as np
+import plotly.express as px
+
+st.set_page_config(page_title="Cartes des BPF",layout="wide", page_icon="🗺️")
+
+
+index=st.session_state['data_bpf'].columns
+
+fig_map = px.scatter_mapbox(st.session_state['data_bpf'], lat=index[4], lon=index[3], hover_name=index[0], hover_data=[index[1], index[2]],
+                        color_discrete_sequence=["blue"], zoom=4)
+fig_map.update_layout(mapbox_style="open-street-map")
+
+st.plotly_chart(fig_map, use_container_width=True, sharing="streamlit")
