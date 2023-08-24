417e992e4986c2c6f351dc3aca8f43a5acb1ef87
Chauve <thomaschauve.sci@disroot.org>
2023-08-24 10:46:52 +0000

    ddd

--- pages/1_Ajouter.py
+++ pages/1_Ajouter.py
@@ -10,7 +10,7 @@ f_date=st.date_input('Date',value=st.session_state['d_date'], label_visibility="
 
 id=list(st.session_state['data_bpf'].Ville).index(add_code)
 
-fig_map = px.scatter_mapbox(st.session_state['data_bpf'].loc[index], lat=index[4], lon=index[3], hover_name=index[0], hover_data=[index[1], index[2]],
+fig_map = px.scatter_mapbox(st.session_state['data_bpf'].loc[id], lat=index[4], lon=index[3], hover_name=index[0], hover_data=[index[1], index[2]],
                         color_discrete_sequence=["blue"], zoom=4)
 fig_map.update_layout(mapbox_style="open-street-map")
 
