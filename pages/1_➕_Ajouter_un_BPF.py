16f52b9e81e2b94b997bb31a896c81d4fca2b50c
Chauve <thomaschauve.sci@disroot.org>
2023-08-25 07:38:50 +0000

    fix

--- pages/1_➕_Ajouter_un_BPF.py
+++ pages/1_➕_Ajouter_un_BPF.py
@@ -21,7 +21,7 @@ id=list(st.session_state['data_bpf'].Ville).index(add_code)
 
 def photo_load(file):
     st.image(file)
-    photo = base64.b64encode(file)
+    photo = base64.b64encode(file.read())
     return photo
 
 uploaded_photo=st.file_uploader('Charger photo', accept_multiple_files=False, label_visibility="visible")
