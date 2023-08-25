0913a7e64a27988cf55f8583f06a0a77804f8af9
Chauve <thomaschauve.sci@disroot.org>
2023-08-25 08:00:06 +0000

    add carte page

---
+++ pages/4_📒_Carte_BPF.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import numpy as np
+import plotly.express as px
+from pathlib import Path
+
+st.set_page_config(page_title="Carte BPF", page_icon="📒")
+
+with st.sidebar:
+    st.session_state['data_bpf'].to_csv('tmp_bpf.csv')
+    st.download_button('Télécharger les données .csv',data=Path('tmp_bpf.csv').read_text(),file_name='list_BPF.csv')
+
+st.title('Carte BPF')
+
+sum_type = st.selectbox('Satistique pour ',np.unique(st.session_state['data_bpf'].Departement))
+
+tmp_df= st.session_state['data_bpf'][st.session_state['data_bpf'].Departement==sum_type]
+tmp=np.array(tmp_df.Ticks)
+tmp_pc=np.sum(tmp)/len(tmp)
+st.progress(tmp_pc, text=pp+': '+str(np.int64(np.sum(tmp)))+'/'+str(len(tmp)))
+
