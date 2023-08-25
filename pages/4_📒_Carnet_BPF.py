3959f82c736459d6cb797793bb81b681dcc70c58
Chauve <thomaschauve.sci@disroot.org>
2023-08-25 08:10:51 +0000

    fix

--- pages/4_📒_Carnet_BPF.py
+++ pages/4_📒_Carnet_BPF.py
@@ -22,7 +22,7 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     for i in [0,1]:
-        st.header(tmp_df.Name.loc[i])
+        st.header(tmp_df.Ville.loc[i])
         if tmp_df.Ticks.loc[i]==1:
             st.write(tmp_df.Date.loc[i])
             if tmp_df.Photo.loc[i] is not None:
@@ -31,7 +31,7 @@ with col1:
 if len(tmp_df)>2:
     with col2:
         for i in [2,3]:
-            st.header(tmp_df.Name.loc[i])
+            st.header(tmp_df.Ville.loc[i])
             if tmp_df.Ticks.loc[i]==1:
                 st.write(tmp_df.Date.loc[i])
                 if tmp_df.Photo.loc[i] is not None:
@@ -39,7 +39,7 @@ if len(tmp_df)>2:
 
     with col3:
         for i in [4,5]:
-            st.header(tmp_df.Name.loc[i])
+            st.header(tmp_df.Ville.loc[i])
             if tmp_df.Ticks.loc[i]==1:
                 st.write(tmp_df.Date.loc[i])
                 if tmp_df.Photo.loc[i] is not None:
